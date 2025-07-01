e8a42d7dfa4c6e2691a3d6873718e25f30e12dc1
ssw1991 <wilson.s.shilo@gmail.com>
2025-07-01 16:46:48 -0400

    Added validation

--- ecom-mono/src/main/java/com/shilo/ecommerce/ecom_mono/controller/CategoryController.java
+++ ecom-mono/src/main/java/com/shilo/ecommerce/ecom_mono/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.shilo.ecommerce.ecom_mono.controller;
 
 import com.shilo.ecommerce.ecom_mono.model.Category;
 import com.shilo.ecommerce.ecom_mono.service.CategoryServiceI;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
         try {
             categoryService.createCategory(category);
             return new ResponseEntity<>("Category added successfully", HttpStatus.OK);
--- ecom-mono/src/main/java/com/shilo/ecommerce/ecom_mono/model/Category.java
+++ ecom-mono/src/main/java/com/shilo/ecommerce/ecom_mono/model/Category.java
@@ -4,17 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-@Entity(name = "categrories")
+@Entity(name = "categories")
 @Data
-@RequiredArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer categoryId;
+    @NotBlank(message = "Category Name cannot be empty")
     private String categoryName;
 }
