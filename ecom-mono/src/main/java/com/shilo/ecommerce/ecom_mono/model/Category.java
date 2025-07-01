c02db9b941cdbb87ac48283ee72c26bf6489538f
ssw1991 <wilson.s.shilo@gmail.com>
2025-07-01 09:19:57 -0400

    Migrated to lombok

--- ecom-mono/src/main/java/com/shilo/ecommerce/ecom_mono/model/Category.java
+++ ecom-mono/src/main/java/com/shilo/ecommerce/ecom_mono/model/Category.java
@@ -4,46 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity(name = "categrories")
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Category {
-
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer categoryId;
     private String categoryName;
-
-    public Category() {
-        this.categoryId = null;
-        this.categoryName = null;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                '}';
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setName(String name) {
-        this.categoryName = name;
-    }
-
-    public Category(Integer categoryId, String name) {
-        this.categoryId = categoryId;
-        this.categoryName = name;
-    }
 }
